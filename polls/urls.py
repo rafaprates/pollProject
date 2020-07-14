e74a563c0b2f0deff7d9985786c7c2174fb249cd
[Rafael Prates] <[rafaelbprates@gmail.com]>
2020-07-14 19:37:31 -0400

    additional setup

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), #displays views.index at polls/
+    path('foo', views.index, name="bar") # displays views.index at polls/foo
 ]
